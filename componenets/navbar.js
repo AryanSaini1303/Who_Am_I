47b041267885d77614a0e2c914ace5aa3f69c25d
AryanSaini1303 <saini.aryan9999@gmail.com>
2025-02-28 23:53:03 +0530

    Feat: Update navbar with link wrapping,  improved image hover effects and added root route anchor tag to it

--- componenets/navbar.js
+++ componenets/navbar.js
@@ -3,12 +3,12 @@ import styles from "@/componenets/navbar.module.css"
 export default function Navbar(){
     return(
         <div className={styles.navbar}>
-            <img src="/images/panda.jpg" alt="" className={styles.circle}/>
+            <a href="/"><img src="/images/panda.jpg" alt="" className={styles.circle}/></a>
             <ul>
-                <li><a href="#Contact">Contact</a></li>
+                <li><a href="#Experience">Experience</a></li>
                 <li><a href="#Achievements">Achievements</a></li>
                 <li><a href="#Testimonials">Testimonials</a></li>
-                <li><a href="#Experience">Experience</a></li>
+                <li><a href="#Contact">Contact</a></li>
             </ul>
             <a href="/Resume.pdf" download className={styles.resume}>Resume</a>
         </div>
