1687816310e8cb232f4d88dcad0bf80c48cd6f0f
AryanSaini1303 <saini.aryan9999@gmail.com>
2025-02-28 23:29:05 +0530

    Feat: Completed the tech stack container of the hero section

--- componenets/heroSection.js
+++ componenets/heroSection.js
@@ -18,14 +18,31 @@ export default function HeroSection() {
           </p>
         </div>
         <div className={styles.imageSection}>
-          <img src="/images/profile3.jpg" alt="" className={styles.profile}/>
-          <img src="/images/profile4.jpg" alt="" className={styles.profile}/>
-          <img src="/images/heart.png" alt="" className={styles.heart}/>
+          <img
+            src="/images/profile3.jpg"
+            alt="profile"
+            className={styles.profile}
+          />
+          <img
+            src="/images/profile4.jpg"
+            alt="profile"
+            className={styles.profile}
+          />
+          <img src="/images/heart.png" alt="heart" className={styles.heart} />
         </div>
       </section>
-      <section
-        className={`${styles.container2} ${styles.childContainer}`}
-      ></section>
+      <section className={`${styles.container2} ${styles.childContainer}`}>
+        <div>
+          <img src="/images/nextjs.png" alt="tech-stack" className={styles.evenImage}/>
+          <img src="/images/python.png" alt="tech-stack" className={styles.oddImage}/>
+          <img src="/images/react-native.png" alt="tech-stack" className={styles.evenImage}/>
+        </div>
+        <div>
+          <img src="/images/vs-code.png" alt="tech-stack" className={styles.evenImage}/>
+          <img src="/images/github.png" alt="tech-stack" className={styles.oddImage}/>
+          <img src="/images/c++.png" alt="tech-stack" className={styles.evenImage}/>
+        </div>
+      </section>
       <section
         className={`${styles.container3} ${styles.childContainer}`}
       ></section>
